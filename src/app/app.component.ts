4f602c8e90b5c115f194db956c136a0cb0e1411c
bclayton-usgs <bclayton@usgs.gov>
2019-02-01 13:33:19 -0700

    Updated for new footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
-import { Navigation } from 'projects/nshmp-ng-template/src/public_api';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import {
+  Navigation,
+  ServiceInfo,
+  FooterService } from 'projects/nshmp-ng-template/src/public_api';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'nshmp-template';
 
   navigationList: Navigation[] = [
@@ -18,5 +22,43 @@ export class AppComponent {
       href: '/'
     }
   ];
+
+  serviceInfo: ServiceInfo = {
+    threads: 1,
+    service: [
+      {
+        name: 'nshmp-haz',
+        url: 'github/nshmp-haz',
+        version: '1'
+      },
+      {
+        name: 'nshmp-haz-ws',
+        url: 'github/nshmp-haz-ws',
+        version: '1'
+      }
+    ]
+  };
+
+  computeSubscription: Subscription;
+  rawDataSubscription: Subscription;
+
+  constructor(private footerService: FooterService) {}
+
+  ngOnInit() {
+    this.computeSubscription = this.footerService.computeButton
+      .subscribe(() => {
+        console.log('Compute button pressed!');
+      });
+
+    this.rawDataSubscription = this.footerService.rawDataButton
+      .subscribe(() => {
+        console.log('Raw data button pressed!');
+      });
+  }
+
+  ngOnDestroy() {
+    this.computeSubscription.unsubscribe();
+    this.rawDataSubscription.unsubscribe();
+  }
 
 }
