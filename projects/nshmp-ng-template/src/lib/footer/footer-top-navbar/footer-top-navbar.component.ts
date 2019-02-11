35505f775a2e2782cdee926d680115cb86ab839b
bclayton-usgs <bclayton@usgs.gov>
2019-02-11 11:59:27 -0700

    Updated footer links to use ngFor

--- projects/nshmp-ng-template/src/lib/footer/footer-top-navbar/footer-top-navbar.component.ts
+++ projects/nshmp-ng-template/src/lib/footer/footer-top-navbar/footer-top-navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FooterLink } from '../footer-link.model';
 
 @Component({
   selector: 'nshmp-template-footer-top-navbar',
@@ -6,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer-top-navbar.component.scss']
 })
 export class FooterTopNavbarComponent implements OnInit {
+
+  footerLinks: FooterLink[] = [
+    {
+      display: 'DOI Privacy Policy',
+      href: 'https://www.doi.gov/privacy'
+    },
+    {
+      display: 'Legal',
+      href: 'https://www.usgs.gov/laws/policies_notices.html'
+    },
+    {
+      display: 'Accessibility',
+      href: 'https://www2.usgs.gov/laws/accessibility.html'
+    },
+    {
+      display: 'Contact USGS',
+      href: 'https://answers.usgs.gov/'
+    },
+    {
+      display: 'Site Map',
+      href: 'https://www.usgs.gov/sitemap.html'
+    },
+  ];
 
   constructor() { }
 
