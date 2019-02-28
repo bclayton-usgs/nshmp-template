7c3f71aa2c1bfb63ac2829a9415adaa3d03b77cc
bclayton-usgs <bclayton@usgs.gov>
2019-02-28 16:28:59 -0700

    No longer expose control panel service

--- projects/nshmp-ng-template/src/public_api.ts
+++ projects/nshmp-ng-template/src/public_api.ts
@@ -6,7 +6,6 @@
 export * from './lib/nshmp-template.service';
 export * from './lib/spinner/spinner.service';
 export * from './lib/header/navigation/navigation.service';
-export * from './lib/main-page/control-panel/control-panel.service';
 
 /* Models */
 export * from './lib/header/navigation/navigation.model';
