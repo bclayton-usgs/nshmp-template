ec168bb97d971585e3a1fef8eb3bbe99438ef1b0
bclayton-usgs <bclayton@usgs.gov>
2019-02-28 12:14:38 -0700

    Updated to show new control panel in template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,7 +25,7 @@ export class AppComponent implements OnInit, OnDestroy {
 
   spinnerSubscription: Subscription;
 
-  fillerContent = Array.from({length: 10}, () =>
+  fillerContent = Array.from({length: 20}, () =>
     `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
     veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
