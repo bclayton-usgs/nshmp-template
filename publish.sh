2034cd9359e6979eeba8a83794b5e76d4211006b
bclayton-usgs <bclayton@usgs.gov>
2019-03-06 08:07:26 -0700

    Remove version bump should do manually

--- publish.sh
+++ publish.sh
@@ -3,12 +3,8 @@
 # Run build script
 npm run build
 
-# Bump version
-cd projects/nshmp-ng-template
-npm version patch
-
 # Lib dir
-cd ../../dist/nshmp-ng-template
+cd dist/nshmp-ng-template
 
 # Publish to npm
 npm publish --access public
