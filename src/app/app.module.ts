69ed363710c95de1ca3835a4138f07d526308dcb
bclayton-usgs <bclayton@usgs.gov>
2019-03-06 15:36:37 -0700

    Add control panel import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import {
   MatInputModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
-import { NshmpTemplateModule } from 'projects/nshmp-ng-template/src/public_api';
+import { NshmpTemplateModule, NshmpTemplateControlPanelModule } from 'projects/nshmp-ng-template/src/public_api';
 import { ControlPanelComponent } from './example-app/control-panel/control-panel.component';
 import { ExampleAppComponent } from './example-app/example-app.component';
 import { Routes, RouterModule } from '@angular/router';
@@ -26,6 +26,7 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     NshmpTemplateModule,
+    NshmpTemplateControlPanelModule,
     MatFormFieldModule,
     MatInputModule,
     RouterModule.forRoot(routes)
