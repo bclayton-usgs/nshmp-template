8dcf8bc6b2d9b854f96c4c62d84a34b1f594787b
bclayton-usgs <bclayton@usgs.gov>
2019-03-07 12:57:58 -0700

    No need for ngIf here

--- projects/nshmp-ng-template/src/lib/gov-banner/gov-banner.component.ts
+++ projects/nshmp-ng-template/src/lib/gov-banner/gov-banner.component.ts
@@ -7,12 +7,6 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class GovBannerComponent implements OnInit {
 
-  /**
-   * Whether to render the US Government banner from USWDS.
-   * Default: true
-   */
-  @Input() renderGovBanner: boolean;
-
   constructor() { }
 
   ngOnInit() {
