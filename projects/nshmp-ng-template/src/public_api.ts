5b05ab988f5933dea40d76ada18bec774ed0b0ee
Brandon Clayton <bclayton@usgs.gov>
2019-03-13 12:56:35 -0600

    Expose checkbox model and form field model

--- projects/nshmp-ng-template/src/public_api.ts
+++ projects/nshmp-ng-template/src/public_api.ts
@@ -10,7 +10,9 @@ export * from './lib/form-field/form-field.service';
 
 /* Models */
 export * from './lib/header/navigation/navigation.model';
+export * from './lib/form-field/form-field.model';
 export * from './lib/form-field/form-button-toggle/form-button-toggle.model';
+export * from './lib/form-field/form-checkbox/form-checkbox.model';
 export * from './lib/form-field/form-input/form-input.model';
 export * from './lib/form-field/form-select/form-select.model';
 export * from './lib/form-field/form-slider/form-slider.model';
