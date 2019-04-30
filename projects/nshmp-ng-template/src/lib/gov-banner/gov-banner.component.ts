9799751e8c4b51d487a6d94abc40a7e95bb0e54c
bclayton-usgs <bclayton@usgs.gov>
2019-04-30 11:25:16 -0600

    Updated to uswds 2.0 release

--- projects/nshmp-ng-template/src/lib/gov-banner/gov-banner.component.ts
+++ projects/nshmp-ng-template/src/lib/gov-banner/gov-banner.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'nshmp-template-gov-banner',
   templateUrl: './gov-banner.component.html',
-  styles: []
+  styleUrls: ['./gov-banner.component.scss']
 })
 export class GovBannerComponent implements OnInit {
 
